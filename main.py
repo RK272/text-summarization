5735709b1a37e5aa8ebec4d8bb243863b1d63ee3
RK272 <rk7918495@gmail.com>
2024-01-13 19:33:13 +0530

    data validation and data ingetion added

---
+++ main.py
@@ -0,0 +1,27 @@
+from textSummarizer.logging import logger
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.pipeline.stage01 import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage2 import DataValidationTrainingPipeline
+
+#logger.info("welcome to our cu log")
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
